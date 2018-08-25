c90f9d045502cd340571c8cd734d4ff2448077fd
eranpeleg1 <eranpeleg1@gmail.com>
2018-08-25 11:50:47 +0300

    adding the styled map

---
+++ components/GoogleAutocomplete.js
@@ -0,0 +1,179 @@
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class GoogleAutocomplete extends React.Component {
+
+
+    render() {
+        return (
+            <GooglePlacesAutocomplete
+                ref={(instance) => { this.GooglePlacesRef = instance }}
+                placeholder='Search here'
+                minLength={1} // minimum length of text to search
+                autoFocus={false}
+                returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+                listViewDisplayed={'false'}    // true/false/undefined
+                fetchDetails={false}
+                predefinedPlaces={[]}
+                currentLocation={false}
+                textInputHide={false}
+                styles={styles}
+                textInputProps={{
+                    onChangeText:(text)=>{
+                            styles.container={flex:0,height:1000};
+                        }
+                    }
+                }
+                renderRightButton={() => {
+                        style={
+                            color:'grey',
+                            marginRight:10
+                        }
+                        return (
+                            <Icon
+                                style={style}
+                                size={20}
+                                name="close"
+                                onPress={() => this.GooglePlacesRef.setAddressText("")}
+                            >
+                            </Icon>)
+                    }
+                }
+
+                renderLeftButton={() => {
+                    style = {
+                        color: 'grey',
+
+                        marginLeft: 10
+                    }
+                    return (
+                        <Icon
+                            style={style}
+                            size={20}
+                            name="arrow-right"
+                            onPress={() =>{
+                                    styles.container={flex:0,height:0};
+                                    this.GooglePlacesRef.triggerBlur();
+                                    this.GooglePlacesRef._onBlur();
+                                    this.GooglePlacesRef.setAddressText('');
+                                }
+                            }
+                        >
+                        </Icon>)
+                    }
+                }
+
+                renderRow={row => {
+
+                    return (
+                        <View style={{flexDirection: 'row'}}>
+                            <Icon name='map-marker' size={20} color={'#5d5d5d'} style={{marginTop: 10}}/>
+                            <View style={{flexDirection: 'column'}}>
+                                <Text style={{
+                                    flex: 1,
+                                    paddingRight: 10,
+                                    fontSize: 15,
+                                    color: 'black'
+                                }} numberOfLines={1}
+                                >{row.structured_formatting.main_text}
+                                </Text>
+                                <Text style={{
+                                    paddingRight: 10,
+                                    fontSize: 13,
+                                    color: '#5d5d5d'
+                                }}>{row.structured_formatting.secondary_text}</Text>
+                            </View>
+                        </View>
+                    );
+                }
+                }
+                renderDescription={row => row.description} // custom description render
+                onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                    console.log(data, details);
+                    this.props.setAddressOfHome(data);
+                    styles.container=styles.container={flex:0,height:0};
+
+                }}
+
+                getDefaultValue={() => ''}
+
+                query={{
+                    // available options: https://developers.google.com/places/web-service/autocomplete
+                    key: 'AIzaSyBKJ88od7dHfemmN5hcCxRpGGABybq_h2k',
+                    language: 'he', // language of the results
+                    types: ["locality", "political", "geocode"] // default: 'geocode'
+                }}
+
+                // currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+                // currentLocationLabel="Current location"
+                nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                GoogleReverseGeocodingQuery={{
+                    // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+                }}
+                GooglePlacesSearchQuery={{
+                    // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                    rankby: 'distance',
+                    types: 'food'
+                }}
+                filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+                // predefinedPlaces={[homePlace, workPlace]}
+                debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+            />);
+    }
+
+}
+const styles = StyleSheet.create({
+    container:{
+        flex:0,height:0
+    },
+    textInput: {
+        height: 40,
+        color: '#5d5d5d',
+        fontSize: 16,
+        paddingBottom:15,
+        margin:0,
+        top:2,
+        textAlign:'right'
+    },
+    listView:{
+      zIndex:11,
+      elevation:11,
+        backgroundColor:"white"
+    },
+    textInputContainer: {
+        backgroundColor: '#ffffff',
+        height: 38,
+        margin:15,
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        elevation: 2,
+        justifyContent:"center",
+        alignItems: 'center',
+    },
+    description: {
+        paddingRight: 10,
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    description2: {
+        paddingRight: 10,
+        fontSize: 13,
+        color: '#5d5d5d'
+    },
+    separator: {
+        height: 1
+    },
+    row: {
+        height: 52,
+        padding: 5
+    }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,188 +1,263 @@
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants, MapView, Location, Permissions } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+export default class HomeScreen extends Component {
+    state = {
+        mapRegion: null,
+        hasLocationPermissions: false,
+        locationResult: null
+    };
 
-export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+    componentDidMount() {
+        this._getLocationAsync();
     }
-  }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+    _handleMapRegionChange = mapRegion => {
+        console.log(mapRegion);
+    };
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                locationResult: 'Permission to access location was denied',
+            });
+        } else {
+            this.setState({ hasLocationPermissions: true });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ locationResult: JSON.stringify(location) });
+
+        // Center the map on the location we just fetched.
+        this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.001, longitudeDelta: 0.001 }});
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>
+                    Pan, zoom, and tap on the map!
+                </Text>
+
+                {
+                    this.state.locationResult === null ?
+                        <Text>Finding your current location...</Text> :
+                        this.state.hasLocationPermissions === false ?
+                            <Text>Location permissions are not granted.</Text> :
+                            this.state.mapRegion === null ?
+                                <Text>Map region doesn't exist.</Text> :
+                                <MapView
+                                    ref={map => this.map = map}
+                                    provider="google"
+                                    customMapStyle={mapStyle}
+                                    style={{ alignSelf: 'stretch', height: 400 }}
+                                    region={this.state.mapRegion}
+                                    showsUserLocation= {true}
+                                    showsMyLocationButton = {false}
+                                    followsUserLocation= {true}
+                                />
+                }
+
+                <Text>
+                    Location: {this.state.locationResult}
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#34495e',
+    },
 });
+
+const mapStyle = [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#3c9bff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#C0ECAD"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#3c9bff"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+];
