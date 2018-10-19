ae6ee8ceac0281dfb88d411ab2ba85980e5305e1
Yoav Lahav <yoavl@wix.com>
2018-10-19 22:13:34 +0300

    fixed warnings on setTimeout at homeScreen first render

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,7 +13,53 @@ import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete"
 
 let {height, width} = Dimensions.get('window');
 
+/* fix for setTimout Issue */
+import {Platform, InteractionManager} from 'react-native';
 
+const _setTimeout = global.setTimeout;
+const _clearTimeout = global.clearTimeout;
+const MAX_TIMER_DURATION_MS = 60 * 1000;
+if (Platform.OS === 'android') {
+// Work around issue `Setting a timer for long time`
+// see: https://github.com/firebase/firebase-js-sdk/issues/97
+    const timerFix = {};
+    const runTask = (id, fn, ttl, args) => {
+        const waitingTime = ttl - Date.now();
+        if (waitingTime <= 1) {
+            InteractionManager.runAfterInteractions(() => {
+                if (!timerFix[id]) {
+                    return;
+                }
+                delete timerFix[id];
+                fn(...args);
+            });
+            return;
+        }
+
+        const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
+        timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
+    };
+
+    global.setTimeout = (fn, time, ...args) => {
+        if (MAX_TIMER_DURATION_MS < time) {
+            const ttl = Date.now() + time;
+            const id = '_lt_' + Object.keys(timerFix).length;
+            runTask(id, fn, ttl, args);
+            return id;
+        }
+        return _setTimeout(fn, time, ...args);
+    };
+
+    global.clearTimeout = id => {
+        if (typeof id === 'string' && id.startWith('_lt_')) {
+            _clearTimeout(timerFix[id]);
+            delete timerFix[id];
+            return;
+        }
+        _clearTimeout(id);
+    };
+}
+/* end fix*/
 export default class HomeScreen extends Component {
     static navigationOptions = {
         header: null,
@@ -83,11 +129,13 @@ export default class HomeScreen extends Component {
         let mapRegion, location, address;
         if (hasLocationPermissions) {
             location = await Location.getCurrentPositionAsync({});
+            console.log(location);
             let tempAddress = await Location.reverseGeocodeAsync(location.coords)
             console.log("address: "+ JSON.stringify(tempAddress))
             tempAddress = tempAddress[0];
             const street = tempAddress.street === null ? "":tempAddress.street
             const name = tempAddress.name === null ? "":tempAddress.name
+
             address = {
                 structured_formatting: {
                     main_text: street + " " + name,
